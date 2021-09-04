3cc82f6c981c1ca67e103a5a18531357e2ccf491
Manuel Torres <manueljosetorres02@gmail.com>
2021-09-04 18:46:04 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -3,13 +3,38 @@ import { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
-// Functional Component
-const Notes = props => props.data.map(note 
-  => <div>{note.text}</div>);
+// Functional Component set it up where notes is the component 
+// props is input, and the array is iterated over. 
+// const Notes = props => props.data.map(note => <div>{note.text}</div>);
+
+const initialData = ["hello", " there,", "ICE CREAM ", "haiudsfh"];
+// https://flexiple.com/react/react-hooks-learn-by-building-a-notes-app/
+const [data, setData]= useState(initialData)
+
+function submitNewNote () {
+  const text=document.querySelector('#takeThis').value
+    // setData((prevData)=> prevData.push(text))
+  
+    if(text) {
+      const nextState = data.push(document.querySelector('#takeThis').value)
+      // console.log(text)
+      setData( prevData => [...prevData])
+      // console.log(data)
+    }
+}
+
+useEffect(()=> {
+// return (data)
+})
 
   return (
     <div className="App">
-      
+      <input id="takeThis" placeholder="enter note" ></input>
+      <button onClick={submitNewNote}>Add Note</button>
+      {/* <Notes data={data} /> */}
+      {data.map(data => {
+        return <h3>{JSON.stringify(data)}</h3>
+      })}
     </div>
   );
 }
